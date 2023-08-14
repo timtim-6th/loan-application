a54b9715c3cc9899d043b71515a8ff1b8cdcd5cb
Honey-badger <timutugi@gmail.com>
2023-08-14 22:56:42 +0300

    complete loan application form

--- script.js
+++ script.js
@@ -1,65 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const calculateEligibilityButton = document.getElementById(
-    "submit"
-  );
-  const modal = document.getElementById("exampleModal");
-  const modalContent = document.querySelector(".modal-body");
-  const closeButton = document.querySelector(".close");
+  const form = document.getElementById("loanForm");
+  const responseMessage = document.getElementById("response");
 
-  calculateEligibilityButton.addEventListener("click", function () {
-    const loanAmount = parseFloat(document.getElementById("exampleInputLoanAmount").value);
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    const desiredLoanAmount = parseFloat(
+      document.getElementById("exampleInputLoanAmount").value
+    );
     const annualIncome = parseFloat(
       document.getElementById("exampleInputAnnualIncome").value
     );
-    const age = calculateAge(new Date(document.getElementById("exampleInputDate").value));
 
-    let eligibilityMessage = "";
-    // age restriction
-    if (age >= 18 && age <= 65) {
-      if (annualIncome >= 30000) {
-        if (loanAmount <= annualIncome * 0.5) {
-          eligibilityMessage =
-            "Congratulations! You are eligible for the loan.";
-        } else {
-          eligibilityMessage =
-            "Sorry, the requested loan amount exceeds your eligibility.";
-        }
-      } else {
-        eligibilityMessage =
-          "Sorry, your annual income is too low for this loan.";
-      }
+    if (isNaN(desiredLoanAmount) || isNaN(annualIncome)) {
+      responseMessage.textContent =
+        "Please fill in valid numbers for both desired loan amount and annual income.";
+    } else if (annualIncome >= desiredLoanAmount) {
+      responseMessage.textContent =
+        "Congratulations! You are eligible for the loan.";
     } else {
-      eligibilityMessage = "Sorry, you are not eligible for a loan.";
+      responseMessage.textContent = "Sorry, you are not eligible for the loan.";
     }
 
-    modalContent.querySelector("#LoanAmntHelpInline").textContent =
-      eligibilityMessage;
-    modal.style.display = "block";
+    // Show the modal with the response
+    const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
+    modal.show();
   });
-
-  closeButton.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
-
-  window.addEventListener("click", function (event) {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-
-  function calculateAge(birthDate) {
-    const today = new Date();
-    const birthDateObj = new Date(birthDate);
-    let age = today.getFullYear() - birthDateObj.getFullYear();
-    const monthDifference = today.getMonth() - birthDateObj.getMonth();
-
-    if (
-      monthDifference < 0 ||
-      (monthDifference === 0 && today.getDate() < birthDateObj.getDate())
-    ) {
-      age--;
-    }
-
-    return age;
-  }
 });
